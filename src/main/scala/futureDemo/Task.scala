a7eb872497a0a1bac190cf6e6fc8f314f79f4c4e
ljy <1404374527@qq.com>
2018-02-03 09:34:51 +0800

    ^@^
    Signed-off-by: ljy <1404374527@qq.com>

--- src/main/scala/futureDemo/Task.scala
+++ src/main/scala/futureDemo/Task.scala
@@ -1,5 +1,31 @@
 package futureDemo
 
-class Task {
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success, Try}//线程池
+
+object Task {
+
+  def main(args: Array[String]): Unit = {
+    for(n1 <- Future{Thread.sleep(2000);println(Thread.currentThread().getName+"1111");1};//两个future是先后顺序执行的，对println的调用发生在内层循环线程中
+        n2 <- Future{println(Thread.currentThread().getName+"222");3} ){
+      println(Thread.currentThread().getName)
+      println(n1+n2)
+    }
+    val order = doInOrder(x=>Future.successful(x+"abc"),s=>Future.successful(s.charAt(1)))
+    println(order(50))
+    Thread.sleep(5000)
+  }
+
+  def doInOrder(f:Int=>Future[String],g:String=>Future[Char]):Int=>Future[Char] ={
+       //val Success(x:String) = f(_:Int).value.get
+    (x:Int) => f(x).value.get match {
+      case Success(y) => g(y)
+      //case Failure(ex) => g(ex)
+    }
+     // ""
+     // g("")
+  }
 
 }
